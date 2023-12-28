fb36db23ab9a77124fd2bcbbb81a89c379595959
Mamadou Aly Sy <sy.aly.mamadou@gmail.com>
2023-12-28 14:50:57 +0000

    feat: :sparkles: Add home page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route(path: '/', name: 'home.index', methods: ['GET'])]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+        return $this->render('index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
